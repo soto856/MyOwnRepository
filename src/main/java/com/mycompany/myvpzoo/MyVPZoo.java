d072e907f243efab1eb74a396ba7b144dd6f0994
soto856 <santiagosotoservin@gmail.com>
2025-10-27 01:22:14 +0100

    first commit

---
+++ src/main/java/com/mycompany/myvpzoo/MyVPZoo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myvpzoo;
+
+/**
+ *
+ * @author santi
+ */
+public class MyVPZoo {
+
+    public static void main(String[] args) {
+        Zoo SherZoo = new Zoo("Welcome to Sheridan Zoo edit!");
+        SherZoo.About();
+    }
+    
+}
---
+++ src/main/java/com/mycompany/myvpzoo/Zoo.java
@@ -0,0 +1,16 @@
+package com.mycompany.myvpzoo;
+
+public class Zoo {
+
+	public String Name;
+
+	public Zoo(String Name) {
+        this.Name = Name;
+	}
+
+	public void About() {
+        System.out.println(Name);
+
+	}
+
+}
